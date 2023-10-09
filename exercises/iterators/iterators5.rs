bd7b83de8cc0c856dfde2300fdd0373126be161f
aucker <aucker@qq.com>
2023-10-09 13:40:39 +0800

    update: finish up to iterator 4
    Signed-off-by: aucker <aucker@qq.com>

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -35,7 +35,10 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // todo!();
+    let mut count = 0;
+    
+    count
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
